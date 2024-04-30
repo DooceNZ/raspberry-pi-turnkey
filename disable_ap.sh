cacc5223b36774403fd25612dadd8897e3a7c92f
DooceNZ <152815936+DooceNZ@users.noreply.github.com>
2024-04-30 20:24:54 +1200

    Update disable_ap.sh
    bookworm

--- disable_ap.sh
+++ disable_ap.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-sleep 3
-
-# disable the AP
-sudo cp config/hostapd.disabled /etc/default/hostapd
-sudo cp config/dhcpcd.conf.disabled /etc/dhcpcd.conf
-sudo cp config/dnsmasq.conf.disabled /etc/dnsmasq.conf
-
-sudo systemctl stop hostapd
-sudo systemctl disable hostapd
-
-sudo reboot now
+nmcli con down WATNEY-AP
+nmcli con delete WATNEY-AP
